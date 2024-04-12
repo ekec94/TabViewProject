482d15a414587b00f4a7bd12f602f7c312d9017a
Emily Kec <ekec@students.mchenry.edu>
2024-04-12 16:41:00 -0500

    Initial commit

---
+++ app/src/main/java/com/example/tabviewproject/ui/profile/ProfileFragment.kt
@@ -0,0 +1,25 @@
+package com.example.tabviewproject.ui.profile
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import com.example.tabviewproject.R
+
+class ProfileFragment : Fragment() {
+
+    private lateinit var profileViewModel: ProfileViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        profileViewModel =
+            ViewModelProvider(this)[ProfileViewModel::class.java]
+
+        return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+}
